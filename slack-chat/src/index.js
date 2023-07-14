7b930d815ab8155eb0128b60172aa97fd2a26180
skyajax <skyajax@github.com>
2023-07-14 17:59:53 +0300

    fix tests errors

--- slack-chat/src/index.js
+++ slack-chat/src/index.js
@@ -2,7 +2,7 @@
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import * as path from 'path';
+// import * as path from 'path';
 // import express from 'express';
 
 import App from './App';
@@ -18,13 +18,13 @@ const app = async () => {
   );
 };
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'), (err) => {
-    // eslint-disable-next-line functional/no-conditional-statements
-    if (err) {
-      res.status(500).send(err);
-    }
-  });
-});
+// app.get('/*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'dist/index.html'), (err) => {
+//     // eslint-disable-next-line functional/no-conditional-statements
+//     if (err) {
+//       res.status(500).send(err);
+//     }
+//   });
+// });
 
 app();
