a42decdea8febea1202c2eab8ca596df803037ff
skyajax <skyajax@github.com>
2023-09-01 12:48:41 +0300

    delete package-lock.json

--- slack-chat/src/components/Spinner.jsx
+++ slack-chat/src/components/Spinner.jsx
@@ -1,7 +1,7 @@
 import { Spinner as BootstrapSpinner } from 'react-bootstrap';
 
 const Spinner = () => (
-  <div className="h-100 d-flex justify-content-center align-items-center">
+  <div className="h-100 position-absolute d-flex justify-content-center align-items-center">
     <BootstrapSpinner animation="border" variant="primary" />
   </div>
 );
